05ea0e690849143a58484e5af6ff17ff6af4a627
Scott Humphries <git@sscotth.io>
2016-02-02 10:33:02 -0600

    simple routing

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ const PORT = process.env.PORT || 3000;
 http.createServer((req, res) => {
   console.log(req.method, req.url);
 
-  res.end('<h1>Hello World!</h1>');
+  if (req.url === '/hello') {
+    res.end('<h1>Hello World!</h1>');
+  } else if (req.url === '/random') {
+    res.end(Math.random().toString());
+  } else {
+    res.writeHead(403);
+    res.end('Access Denied!');
+  }
+
 }).listen(PORT, () => {
   console.log(`Node.js server started. Listening on port ${PORT}`);
 });

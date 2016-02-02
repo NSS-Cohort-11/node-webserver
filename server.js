f78f61e9bbd7f34910aaae933adb5b20a5de14f4
Scott Humphries <git@sscotth.io>
2016-02-02 10:33:52 -0600

    chunked responses

--- server.js
+++ server.js
@@ -7,7 +7,24 @@ http.createServer((req, res) => {
   console.log(req.method, req.url);
 
   if (req.url === '/hello') {
-    res.end('<h1>Hello World!</h1>');
+    const msg = `<h1>Hello World!</h1>
+<h2>Goodbye World!</h2>`;
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+
+    // chunk response by character
+    msg.split('').forEach((char, i) => {
+      setTimeout(() => {
+        res.write(char);
+      }, 1000 * i);
+    });
+
+    // wait for all characters to be sent
+    setTimeout(() => {
+      res.end();
+    }, msg.length * 1000 + 2000);
   } else if (req.url === '/random') {
     res.end(Math.random().toString());
   } else {

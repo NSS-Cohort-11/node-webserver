38c660441e0e66c2487736856459ca4807e97ebd
Scott Humphries <git@sscotth.io>
2016-02-11 15:38:22 -0600

    eod

--- server.js
+++ server.js
@@ -3,18 +3,108 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const upload = require('multer')({ dest: 'tmp/uploads' });
+const request = require('request');
+const _ = require('lodash');
+const cheerio = require('cheerio');
+const MongoClient = require('mongodb').MongoClient;
 
 const PORT = process.env.PORT || 3000;
+const MONGODB_URL = 'mongodb://localhost:27017/node-webserver';
+
+let db;
 
 app.set('view engine', 'jade');
 
 app.locals.title = 'THE Super Cool App';
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    date: new Date
+  db.collection('news').findOne({}, {sort: {_id: -1}}, (err, doc) => {
+    if (err) throw err;
+
+    res.render('index', {
+      date: new Date(),
+      topStory: doc.top[0]
+    });
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.send({hello: 'world'});
+});
+
+app.post('/api', (req, res) => {
+  const obj = _.mapValues(req.body, val => val.toUpperCase());
+
+  db.collection('allcaps').insertOne(obj, (err, result) => {
+    if (err) throw err;
+
+    res.send(result.ops[0]);
+  });
+});
+
+app.get('/api/weather', (req, res) => {
+  const API_KEY = '00c2032f84f5e9393b7a1eda02d49228';
+  const url = `https://api.forecast.io/forecast/${API_KEY}/37.8267,-122.423`;
+
+  request.get(url, (err, response, body) => {
+    if (err) throw err;
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.send(JSON.parse(body));
+  });
+});
+
+app.get('/api/news', (req, res) => {
+  db.collection('news').findOne({}, {sort: {_id: -1}}, (err, doc) => {
+    if (err) throw err;
+
+    if (doc) {
+      const FIFTEEN_MINUTES_IN_MS = 15 * 60 * 1000;
+      const diff = new Date() - doc._id.getTimestamp() - FIFTEEN_MINUTES_IN_MS;
+      const lessThan15MinutesAgo = diff < 0;
+
+      if (lessThan15MinutesAgo) {
+        res.send(doc);
+        return;
+      }
+    }
+
+    const url = 'http://cnn.com';
+
+    request.get(url, (err, response, html) => {
+      if (err) throw err;
+
+      const news = [];
+      const $ = cheerio.load(html);
+
+      const $bannerText = $('.banner-text');
+
+      news.push({
+        title: $bannerText.text(),
+        url: url + $bannerText.closest('a').attr('href')
+      });
+
+      const $cdHeadline = $('.cd__headline');
+
+      _.range(1, 12).forEach(i => {
+        const $headline = $cdHeadline.eq(i);
+
+        news.push({
+          title: $headline.text(),
+          url: url + $headline.find('a').attr('href')
+        });
+      });
+
+      db.collection('news').insertOne({ top: news }, (err, result) => {
+        if (err) throw err;
+
+        res.send(news);
+      });
+    });
   });
 });
 
@@ -23,9 +113,17 @@ app.get('/contact', (req, res) => {
 });
 
 app.post('/contact', (req, res) => {
-  const name = req.body.name;
+  const obj = {
+    name: req.body.name,
+    email: req.body.email,
+    message: req.body.message
+  };
 
-  res.send(`<h1>Thanks for contacting us ${name}</h1>`);
+  db.collection('contact').insertOne(obj, (err, result) => {
+    if (err) throw err;
+
+    res.send(`<h1>Thanks for contacting us ${obj.name}</h1>`);
+  });
 });
 
 app.get('/sendphoto', (req, res) => {
@@ -75,12 +173,18 @@ app.get('/secret', (req, res) => {
     .send('Access Denied!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Node.js server started. Listening on port ${PORT}`);
+MongoClient.connect(MONGODB_URL, (err, database) => {
+  if (err) throw err;
+
+  db = database;
+
+  app.listen(PORT, () => {
+    console.log(`Node.js server started. Listening on port ${PORT}`);
+  });
 });
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // Returns a random integer between min (included) and max (excluded)
-function getRandomInt(min, max) {
+function getRandomInt (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
